faee771ef4509312f44ed4fed8307b033a641aa1
Ivan-Romanovsky <itromanovsky@gmail.com>
2025-05-03 16:09:25 +0600

    Initial commit with UI tests

--- src/test/java/UItests.java
+++ src/test/java/UItests.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UItests {
 WebDriver driver;
@@ -18,7 +22,16 @@ public void setUp()
 }
 @Test
 public void testTriangle(){
+    WebElement surrenderButton = driver.findElement(By.xpath("//button[text()='Я сдаюсь']"));
+    assertNotNull(surrenderButton, "Кнопка 'Я сдаюсь' не найдена на странице");
 
+    surrenderButton.click();
+
+    WebElement answerLink = driver.findElement(By.xpath("//a[text()='Ссылка на ответы']"));
+    assertNotNull(answerLink, "Ссылка 'Ссылка на ответы' не найдена на странице");
+
+    WebElement hideButton = driver.findElement(By.xpath("//button[text()='Спрятать ответы']"));
+    assertNotNull(hideButton, "Кнопка 'Спрятать ответы' не найдена на странице");
     }
 @AfterEach
 public void tearDown(){
